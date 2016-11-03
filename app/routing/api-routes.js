2ede0917346df086a17346e73dd948bb7a60f873
clshu <ccss0630@yahoo.com>
2016-11-03 04:33:49 -0500

    Add POST  '/api/friends' hndler to app/routing/api-routes.js

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -6,4 +6,36 @@ module.exports = function (app) {
 	app.get('/api/friends', function (req, res) {
 		res.json(friends);
 	});
+	app.post('/api/friends', function (req, res) {
+		var match = findMatch(req.body, friends);
+		res.json(match);
+	});
+
+	function findDifferences(scores1, scores2) {
+		var diffs = 0;
+		for (var i = 0; i < scores1.length; i++) {
+			diffs += Math.abs(Number(scores1[i]) - Number(scores2[i]));
+		}
+
+		return diffs;
+	}
+	function findMatch(user, friends) {
+		// 10 numbers in scores, so larget diffs between 2 arrays
+		// is 10 * (5 - 1) = 40
+		var lowestDiff = 40; 
+		var match = null;
+
+		for (var i = 0; i < friends.length; i++) {
+			var diffs = findDifferences(user.scores, friends[i].scores);
+			//console.log(friends[i].name + ': ' + diffs);
+			if (diffs < lowestDiff) {
+				lowestDiff = diffs;
+				match = friends[i];
+			}
+		};
+
+		friends.push(user);
+		return match;
+	}
+
 }
