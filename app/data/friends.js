a8c869b2b92319806023802c2149d833647058cc
clshu <ccss0630@yahoo.com>
2016-11-04 02:20:54 -0500

    Replace a broken URL link with a placehold.it link

--- app/data/friends.js
+++ app/data/friends.js
@@ -83,7 +83,7 @@ scores: [
 },
 {
 name: "Jordan Biason",
-photo: "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+photo: "https://placehold.it/200x200",
 scores: [
 4,
 4,
